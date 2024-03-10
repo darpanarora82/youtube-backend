6291d33febdbe4a63f4e804f564354dd64573cac
Chirag Arora <darpanarora82@gmail.com>
2024-03-10 15:29:06 +0530

    repo initialized

---
+++ src/main/java/com/youtubebackend/youtubebackend/YoutubeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youtubebackend.youtubebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtubebackend/youtubebackend/YoutubeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubebackend.youtubebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
