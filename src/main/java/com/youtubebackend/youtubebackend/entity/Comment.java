231c7c532a0a734594db727233c7de732dc0bb88
Akash Gupta <gr8akash1994@gmail.com>
2024-03-10 20:01:46 +0530

    Changes

--- src/main/java/com/youtubebackend/youtubebackend/entity/Comment.java
+++ src/main/java/com/youtubebackend/youtubebackend/entity/Comment.java
@@ -1,64 +1,16 @@
 package com.youtubebackend.youtubebackend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Comment")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
-    public String getId() {
-        return id;
-    }
-
-    public Comment() {
-    }
-
-    public Comment(String id, String text, String author, Long likeCount, Long dislikeCount) {
-        this.id = id;
-        this.text = text;
-        this.author = author;
-        this.likeCount = likeCount;
-        this.dislikeCount = dislikeCount;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Long getLikeCount() {
-        return likeCount;
-    }
-
-    public void setLikeCount(Long likeCount) {
-        this.likeCount = likeCount;
-    }
-
-    public Long getDislikeCount() {
-        return dislikeCount;
-    }
-
-    public void setDislikeCount(Long dislikeCount) {
-        this.dislikeCount = dislikeCount;
-    }
-
     @Id
     private String id;
     private String text;
--- src/main/java/com/youtubebackend/youtubebackend/repository/VideoRepository.java
+++ src/main/java/com/youtubebackend/youtubebackend/repository/VideoRepository.java
@@ -2,6 +2,8 @@ package com.youtubebackend.youtubebackend.repository;
 
 import com.youtubebackend.youtubebackend.entity.Video;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VideoRepository extends MongoRepository<Video, String> {
 }
