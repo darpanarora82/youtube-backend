5fada06b2ac25444caa3778f9505ca0b55c60ea3
Chirag Arora <darpanarora82@gmail.com>
2024-03-11 17:37:47 +0530

    Code refactored

---
+++ src/main/java/com/youtubebackend/youtubebackend/config/AwsS3Configuration.java
@@ -0,0 +1,33 @@
+package com.youtubebackend.youtubebackend.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Configuration {
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        //Create AmazonS3 Object for the same with credentials and region
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.fromName(region))
+                .build();
+    }
+}
--- src/main/java/com/youtubebackend/youtubebackend/service/S3UploadDownloadService.java
+++ src/main/java/com/youtubebackend/youtubebackend/service/S3UploadDownloadService.java
@@ -6,52 +6,36 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import com.youtubebackend.youtubebackend.config.AwsS3Configuration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 
 
 @Service
 @RequiredArgsConstructor
 public class S3UploadDownloadService {
-
-    @Value("${aws.accessKey}")
-    private String accessKey;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
-
-
+    @Autowired
+    public AwsS3Configuration amazonS3Configuration;
     public static final String bucketName = "video-0-0";
 
-
     public String uploadFile(MultipartFile multipartFile) throws IOException {
-
-       //Create Credentials using BasicAWSCredentials
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        //Create AmazonS3 Object for the same with credentials and region
-        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .withRegion(Regions.fromName(region))
-                .build();
-
         //File name For Multipart File
-        var fileName= multipartFile.getOriginalFilename();
+        var fileName = multipartFile.getOriginalFilename();
 
         var objectMetadata = new ObjectMetadata();
 
         objectMetadata.setContentLength(multipartFile.getSize());
         objectMetadata.setContentType(multipartFile.getContentType());
 
-            PutObjectRequest putObjectResult = new PutObjectRequest(bucketName,fileName,
-                    multipartFile.getInputStream(), objectMetadata);
+        PutObjectRequest putObjectResult = new PutObjectRequest(bucketName, fileName,
+                multipartFile.getInputStream(), objectMetadata);
 
+        AmazonS3 amazonS3 = amazonS3Configuration.amazonS3();
         amazonS3.putObject(putObjectResult);
         return amazonS3.getUrl(bucketName, fileName).toString();
     }
